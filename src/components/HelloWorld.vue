5aa623dfa37b0cf381d466c934cb1b4c7f5908fc
qdweng <1271737399@qq.com>
2020-08-03 21:13:10 +0800

    u删除没必要的

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(item,index) in pics" :key="index">
+        <img :src="item" alt @mouseenter="showPicMsg(index)" @mouseout="hidePicMsg()" />
+        <span
+          v-if="currentindex==index&&currentindex>-1"
+          class="showPX"
+        >{{picsInfo[index].width}}✖{{picsInfo[index].height}}</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  //比较简单,请别见笑。
+  data() {
+    return {
+      currentindex: -1,
+      pics: [
+        "https://t7.baidu.com/it/u=378254553,3884800361&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597047628&t=ed92ee981e56cfb90c27bbdfb470f8a5",
+        "https://t8.baidu.com/it/u=3571592872,3353494284&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597047628&t=fa08de43f5e63a01c3ca1174a41fd138",
+        "https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597047628&t=df626c9b5cfa9c7ea8ef9d89a3c7a0b2",
+        "https://t8.baidu.com/it/u=1484500186,1503043093&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597047628&t=3606a8b89d25f7fd8e4c05565e2f02a1",
+        "https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597047628&t=c43ee834b29068077aa94949590d5998",
+        "https://t9.baidu.com/it/u=1761131378,1355750940&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597062700&t=acb179176b24ef8c5464546745dc0748",
+        "https://t9.baidu.com/it/u=86853839,3576305254&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1597062700&t=023fa31e6cadc9f512579c73a021ac1c",
+      ],
+      picsInfo: [],
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    // 获取图片像素
+    getImgInfo() {
+      let imgInfo = {};
+      for (let i = 0; i < this.pics.length; i++) {
+        let img = new Image();
+        img.src = this.pics[i];
+        img.onload = function () {
+          imgInfo.width = img.width;
+          imgInfo.height = img.height;
+          return imgInfo;
+        };
+        this.picsInfo.push(imgInfo);
+      }
+    },
+    // 鼠标移入
+    showPicMsg(index) {
+      this.currentindex = index;
+      console.log("鼠标意移入");
+    },
+    //鼠标移出
+    hidePicMsg() {
+      this.currentindex = -1;
+    },
+  },
+  mounted() {
+    this.getImgInfo();
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 h3 {
   margin: 40px 0 0;
@@ -50,9 +75,19 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
+}
+li {
+  position: relative;
+  .showPX {
+    position: absolute;
+    left: 30px;
+    bottom: 30px;
+    opacity: 0.8;
+    background-color: whitesmoke;
+    color: black;
+  }
 }
 </style>
